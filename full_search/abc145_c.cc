8ddc14b23e8eae2253ea2454d20af15486b7e931
Aki T <84285825+akt00@users.noreply.github.com>
2022-06-21 16:57:37 +0900

    Update abc145_c.cc

--- full_search/abc145_c.cc
+++ full_search/abc145_c.cc
@@ -3,12 +3,11 @@
 #include <cmath>
 #include<algorithm>
 using namespace std;
-// permutation full search
 
 
-double dist(int x1, int y1, int x2, int y2) {
+inline float dist(int x1, int y1, int x2, int y2) {
 
-    return sqrt(pow(x1 - x2, 2) + pow(y1-y2, 2));
+    return (float)sqrt(pow(x1 - x2, 2) + pow(y1-y2, 2));
 }
 
 int main() {
@@ -23,25 +22,20 @@ int main() {
         cin >> point[1];
         points.push_back(point);
     }
-    cout << endl;
-    
-    double total_dist = 0;
-    int count = 0;
+
     std::sort(points.begin(), points.end());
-  
+    for (auto i : points) {
+        for (auto j : i) cout << j << " ";
+        cout << endl;
+    }
+    float total_dist = 0;
+    int count = 0;
     while (true) {
-        cout << ++count << endl;
+        count++;
         for (auto i = 0; i < n - 1;i++) {
             total_dist += dist(points[i][0], points[i][1], points[i+1][0], points[i+1][1]);
         }
-
-
-        for (auto i : points) {
-            for (auto j : i) cout << j << " ";
-            cout << endl;
-        }
-        cout << endl;
         if(next_permutation(points.begin(), points.end()) == false) break;
     }
-    cout << "avg total = " << total_dist/6<< endl;
+    cout << total_dist / (float)count << endl;
 }
