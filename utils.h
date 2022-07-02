26027458808466844a3dfe9da340f42c7db123cc
Aki T <84285825+akt00@users.noreply.github.com>
2022-07-02 23:46:12 +0900

    Update utils.h

--- utils.h
+++ utils.h
@@ -19,3 +19,55 @@ void ParseConsecutiveInt(vector<int>& row, int input) {
 		if (input <= 0) return;
 	}
 }
+
+
+// move matrix cell by 1 in one of 8 directions, overflow-proof
+pair<int, int> NextIndex(int dir, int n, const pair<int, int>& p) {
+
+	// cout << p.first << " " << p.second << endl;
+	// dir 0 = right;
+	auto copy = p;
+	if (dir == 0) {
+		copy.second += 1;
+		if (copy.second == n) copy.second = 0;
+	}
+	if (dir == 1) {
+		copy.first += 1;
+		if (copy.first == n) copy.first = 0;
+		copy.second += 1;
+		if (copy.second == n) copy.second = 0;
+	}
+	if (dir == 2) {
+		copy.first += 1;
+		if (copy.first == n) copy.first = 0;
+	}
+	if (dir == 3) {
+		copy.first -= 1;
+		if (copy.first < 0) copy.first = n-1;
+		copy.second += 1;
+		if (copy.second == n) copy.second = 0;
+	}
+	if (dir == 4) {
+		copy.second -= 1;
+		if (copy.second < 0) copy.second = n - 1;
+	}
+	if (dir == 5) {
+		copy.first -= 1;
+		if (copy.first < 0) copy.first = n-1;
+		copy.second -= 1;
+		if (copy.second < 0) copy.second = n - 1;
+	}
+	if (dir == 6) {
+		copy.first -= 1;
+		if (copy.first < 0) copy.first = n - 1;
+	}
+	if (dir == 7) {
+		copy.first -= 1;
+		if (copy.first < 0) copy.first = n-1;
+		copy.second += 1;
+		if (copy.second == n) copy.second = 0;
+	}
+
+	// cout << copy.first << " " << copy.second << endl;
+	return copy;
+}
